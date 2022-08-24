cbfee8c3f12f250794ecd6b393dd9ea4d9a5b3a7
Alian Medina <alianmc1988@gmail.com>
2022-08-24 11:35:06 -0300

    env variables for twilio

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
-require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const ROOT_URL = process.env.ROOT_URL || `/`;
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID || ``;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN || ``;
 
 module.exports = {
   PORT,
   ROOT_URL,
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
 };
